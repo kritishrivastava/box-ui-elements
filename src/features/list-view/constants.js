b0f952b4872a7ca9bd61e912d3439854f517b6c2
Justin Holdstock <jholdstock@box.com>
2019-03-28 16:47:49 -0600

    build(examples): v10.0.0-beta.18

--- src/features/list-view/constants.js
+++
@@ -1,9 +0,0 @@
-const ROW_HEIGHT = 50;
-const FIXED_ROW_COUNT = 1;
-
-const NEED_REFINING = 'needRefining';
-const TOO_MANY_RESULTS = 'tooManyResults';
-const NO_ACCESS_FOR_QUERY = 'noAccessForQuery';
-const NO_ACCESS_FOR_TEMPLATE = 'noAccessForTemplate';
-
-export { FIXED_ROW_COUNT, ROW_HEIGHT, NEED_REFINING, TOO_MANY_RESULTS, NO_ACCESS_FOR_QUERY, NO_ACCESS_FOR_TEMPLATE };
